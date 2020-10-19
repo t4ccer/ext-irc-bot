d1dc8801d6608f425263ad9f58656175de3c7b53
t4ccer <64430288+t4ccer@users.noreply.github.com>
2020-10-19 17:17:30 +0200

    Make EchoBot main program

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,6 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-
 module Main where
 
-import           ChatEvents
-import           Data.String.Interpolate (i)
-import           ExtIrcBot
-import           Utils
-
-botName :: String
-botName = "foobot"
+import qualified Examples.EchoBot as EchoBot
 
 main :: IO ()
-main = runBot BotSettings
-  { host ="irc.freenode.net"
-  , port = "6667"
-  , name = botName
-  , channels = ["#botwar"]
-  , handler =  handleEvent
-  , async_handlers = False
-  }
-
-handleEvent :: Handler
-handleEvent (UserJoined u c)       = return $ SendChannelMessage c [i|Hello #{humanizeName u}|]
-handleEvent (ChannelMessage u c m) = return $ SendChannelMessage c [i|#{humanizeName u} said: #{m}|]
-handleEvent _                      = return $ NoAction
+main = EchoBot.run
